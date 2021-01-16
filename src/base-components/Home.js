92567c03af749066679f38ec5b725040f433bce7
Benjamin S <ben.sommer.skovgaard@gmail.com>
2021-01-16 20:33:50 +0100

    first

---
+++ src/base-components/Home.js
@@ -0,0 +1,12 @@
+import { API_URL } from "../utils/settings";
+
+export let URL = API_URL;
+
+export default function Home() {
+
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
---
+++ src/utils/settings.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://bencat.dk/exam-backend";
